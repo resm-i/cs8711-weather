d9bd2e306bcb9ae5544a3711a2b52885c8552874
resm-i <130730318+resm-i@users.noreply.github.com>
2023-11-14 16:47:50 +0530

    Add files via upload

---
+++ CS8711-main/CS8711-main/sample/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+  
+@app.route('/', methods =["GET", "POST"])
+def index(): 
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":       
+        cityName = request.form.get("cityName")  
+        if cityName:
+            weatherApiKey = 'feb3114b567f702e07604b79944db508'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+        else:
+            error = 1    
+    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=8000,debug=True)
